495aa7f6076ce5ba374a1070591208bda4881d90
Dysar <david0zin3@gmail.com>
2023-11-16 14:54:45 +0200

    initial

---
+++ src/main/java/com/example/mintos/dto/response/ResponseError.java
@@ -0,0 +1,12 @@
+package com.example.mintos.dto.response;
+
+
+import java.time.LocalDateTime;
+
+public class ResponseError {
+
+	private LocalDateTime timestamp;
+	
+	private String details;
+
+}
---
+++ src/test/java/com/example/mintos/MintosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mintos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
